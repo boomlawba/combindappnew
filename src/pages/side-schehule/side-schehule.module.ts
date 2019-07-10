7ab7bc7b0af11198a5a5719f204524a2f51895f4
John Doe <johndoe@example.com>
2019-07-10 13:16:44 +0700

    menu complete

---
+++ src/pages/side-schehule/side-schehule.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SideSchehulePage } from './side-schehule';
+
+@NgModule({
+  declarations: [
+    SideSchehulePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SideSchehulePage),
+  ],
+})
+export class SideSchehulePageModule {}
